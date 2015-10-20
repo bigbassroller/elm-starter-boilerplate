c66c4fa487157f5a1ba4b1b744fdf2ed22390dd6
Michael Chavez <bigbassroller@gmail.com>
2015-10-20 12:11:21 -0700

    added elm files to watch task

--- gulp/watch.js
+++ gulp/watch.js
@@ -5,6 +5,9 @@ Gulp.task('watch', function () {
 
     global.isWatching = true;
     Gulp.watch('./*.elm', ['elm']);
+    Gulp.watch('./client/*.elm', ['elm']);
+    Gulp.watch('./client/*/*.elm', ['elm']);
+    Gulp.watch('./client/*/*/*.elm', ['elm']);
     Gulp.watch('./client/*.scss', ['sass']);
     Gulp.watch('./client/**/*.scss', ['sass']);
 });
